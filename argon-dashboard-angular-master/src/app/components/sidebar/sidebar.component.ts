0b871342355bff46eb48d1a4b856bf3d5681839a
trifyindia <trifyindia@trify.us>
2023-05-26 23:48:29 +0530

    added all lost file and now this is final

--- argon-dashboard-angular-master/src/app/components/sidebar/sidebar.component.ts
+++ argon-dashboard-angular-master/src/app/components/sidebar/sidebar.component.ts
@@ -14,7 +14,7 @@ export const ROUTES: RouteInfo[] = [
     // { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
     // { path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
     { path: '/admin', title: 'Profile Tag',  icon:'ni-single-02 text-yellow', class: '' },
-    { path: '/login', title: 'Log Out',  icon:'ni-single-02 text-yellow', class: '' },
+    { path: '/landing', title: 'Log Out',  icon:'ni-single-02 text-yellow', class: '' },
     // { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
     // { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' },
     // { path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' }
